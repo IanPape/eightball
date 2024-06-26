1ffb51c05f3d8b5a54dfe0e2e10fb7f3b6456d12
Ian Pape <ianjamespape@gmail.com>
2024-06-26 09:49:51 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EightBall from './EightBall'
+
+function App () {
+    return (
+        <div>
+            <EightBall/>
+        </div>
+    );
+}
+
+
+export default App;
